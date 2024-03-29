f95992953bb98c38bb8eaf94907d88a7f00dcac9
AvKov <37872714+dupitydumb@users.noreply.github.com>
2024-03-29 18:15:42 +0700

    first fixs

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ function App() {
         Welcome to Our Chrome Extension
       </h1>
       <img
-        src="src/assets/icon.png"
+        src="https://raw.githubusercontent.com/dupitydumb/NoAffiliate/main/src/assets/icon.png"
         alt="Extension Icon"
         className="w-32 h-32 mb-4"
       />
       <p className="mb-4 text-lg">Anti shopee affiliator spammmmmmm</p>
       <div className="p-10 bg-white rounded-lg shadow-2xl max-w-md w-full mb-4 text-black">
         <h2 className="text-3xl font-bold mb-4">Installation Instructions</h2>
-        <img src="src/assets/tutorial.gif" alt="Step 1" className="mb-4" />
+        <img
+          src="https://github.com/dupitydumb/NoAffiliate/blob/main/src/assets/tutorial.gif?raw=true"
+          alt="Step 1"
+          className="mb-4"
+        />
         <ol className="list-decimal list-inside"></ol>
       </div>
       <a
-        href="src/assets/AntiAffiliate.zip"
+        href="https://github.com/dupitydumb/NoAffiliate/raw/main/src/assets/AntiAffiliate.zip"
         download
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
